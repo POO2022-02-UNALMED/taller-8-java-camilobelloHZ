08518c8f7fce83506b8eae20faa970e0658b25fe
camilobelloHZ <cbelloh@unal.edu.co>
2022-10-21 01:19:02 -0500

    prueba 3

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -34,7 +34,12 @@ public class Jugador extends Futbolista {
     
     @Override
     public String toString() {
-        return "El futbolista "+this.getNombre()+" tiene "+this.getEdad()+", y juega de "+this.getPosicion()+" con el dorsal "+this.dorsal+". Ha marcado "+this.golesMarcados;
+        return "El futbolista "
+        		+this.getNombre()+
+        		" tiene "+this.getEdad()+
+        		", y juega de "+this.getPosicion()+
+        		" con el dorsal "+this.dorsal+
+        		". Ha marcado "+this.golesMarcados;
     }
     
     @Override
